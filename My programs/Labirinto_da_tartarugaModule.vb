cc55f17ea1610594896dfc46a7b3501578ad7f1d
Thiago Sousa <thiagosousa81@gmail.com>
2023-03-21 21:40:33 -0300

    Add files via upload

---
+++ My programs/Labirinto_da_tartarugaModule.vb
@@ -0,0 +1,22 @@
+Module Labirinto_da_tartarugaModule
+    Sub Main()
+        'Criando o gráfico;
+        'C:\Users\Ridalva\Documents\Corporation_\Small Basic
+        GraphicsWindow.BackgroundColor = "darkgreen"
+        GraphicsWindow.CanResize = false
+        Turtle.Show()
+        Turtle.PenUp()
+        Turtle.MoveTo(10, 10)
+        Turtle.Turn(140)
+        'Criando o labirinto;
+
+        GraphicsWindow.DrawLine(0, 25, 600, 25)
+
+
+        '
+
+
+        'Condições
+
+    End Sub
+End Module
